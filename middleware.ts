61d7f3ed25bdc6fe7cff3e8bb11057b5f3c6a615
LuisVarela <luis.fernando.varela.navarro@gmail.com>
2024-10-02 13:28:47 -0500

    ajuste de middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,5 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/", "/projects","/projects"] };
+export const config = {
+  matcher: ["/", "/projects", "/projects/manage"],
+};
