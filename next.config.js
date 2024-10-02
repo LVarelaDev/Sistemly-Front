30e74e97a04ab9b2b121d679589cf156e17f45c4
LuisVarela <luis.fernando.varela.navarro@gmail.com>
2024-10-02 14:02:10 -0500

    desactivado el middleware

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        forceSwcTransforms: true, // Desactiva Edge Functions
-      },
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//         forceSwcTransforms: true, // Desactiva Edge Functions
+//       },
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
