be42bf3d7a1585c3c37856acc2cb818cd0a1e9f6
LuisVarela <luis.fernando.varela.navarro@gmail.com>
2024-10-02 13:35:35 -0500

    master

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        forceSwcTransforms: true, // Desactiva Edge Functions
+      },
+}
 
 module.exports = nextConfig
