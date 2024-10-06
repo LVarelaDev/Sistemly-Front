541412579d2c50fac954b6b1cedef8ac83ec6bab
LuisVarela <luis.fernando.varela.navarro@gmail.com>
2024-10-05 22:16:05 -0500

    se implementan adiciones de materiales

--- components/pages/projects/project-details/partials/economic-proposal/partials/TableHeader.tsx
+++ components/pages/projects/project-details/partials/economic-proposal/partials/TableHeader.tsx
@@ -6,36 +6,45 @@ import {
   DropdownMenu,
   DropdownTrigger,
 } from "@nextui-org/react";
-import { FilterIcon } from "hugeicons-react";
+import { FilterIcon, PlusSignIcon } from "hugeicons-react";
 import React from "react";
 import { UseFormReturn } from "react-hook-form";
+import ModalAdd from "./ModalAdd";
+import { KeyedMutator } from "swr";
+import { EconomicProposalDto } from "@/models/projects/economicProposal";
+import { ProjectDto } from "@/models/projects/projectDto";
 
 interface Props {
   form: UseFormReturn<any, any>;
+  projectId: number;
+  mutate: KeyedMutator<EconomicProposalDto[]>;
+  mutateProject: KeyedMutator<ProjectDto>;
 }
 
-const TableHeader = ({ form }: Props) => {
+const TableHeader = ({ form, projectId, mutate, mutateProject }: Props) => {
   return (
-    <div className="flex justify-end items-center gap-x-3">
-      <div className="w-1/2">
-        <InputSearch
-          form={form}
-          name="searchValue"
-          placeholder="Buscar nombre de material"
-        />
+    <section className="flex items-center justify-between py-3">
+      <ModalAdd
+        projectId={projectId}
+        mutate={mutate}
+        mutateProject={mutateProject}
+      />
+
+      <div className="flex items-center gap-x-3 w-1/2">
+        <InputSearch form={form} name="searchValue" placeholder="Buscar..." />
+        <Dropdown>
+          <DropdownTrigger>
+            <Button isIconOnly variant="flat" color="primary">
+              <FilterIcon />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Selection sort" selectionMode="single">
+            <DropdownItem key="desc">Ultimos equipos agregados</DropdownItem>
+            <DropdownItem key="asc">Primeros equipos agregados</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </div>
-      <Dropdown>
-        <DropdownTrigger>
-          <Button isIconOnly variant="flat" color="primary">
-            <FilterIcon />
-          </Button>
-        </DropdownTrigger>
-        <DropdownMenu aria-label="Selection sort" selectionMode="single">
-          <DropdownItem key="desc">Ultimos equipos agregados</DropdownItem>
-          <DropdownItem key="asc">Primeros equipos agregados</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-    </div>
+    </section>
   );
 };
 
